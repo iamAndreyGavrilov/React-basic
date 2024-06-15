69469b93c974f726c68ebf07ea1575be6d724ded
A G <a.gavrilof@zordoc.uz>
2024-06-15 17:47:32 +0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h3>Header</h3>
+        <span>
+          <a href="#">Home</a>
+        </span>
+      </header>
+
+      <main>
+        <h1>Hello React</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
