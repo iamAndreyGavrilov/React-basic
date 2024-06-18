ad3caaaa8ef3d8f2f10f050928077cd9f0fb2fe1
A G <a.gavrilof@zordoc.uz>
2024-06-18 15:40:48 +0500

    add IntroSection

---
+++ src/components/IntroSection.tsx
@@ -0,0 +1,13 @@
+export default function IntroSection() {
+  return (
+    <section>
+      <h1 className="centered">Intro</h1>
+      <h3 className="centered" style={{ color: 'grey' }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in dui mauris. Vivamus
+        hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend
+        nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis
+        nisl tempor.
+      </h3>
+    </section>
+  );
+}
